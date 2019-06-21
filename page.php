12fea0aca6956318b9fcde4a60b3dfbcd7d9fa82
Aleksey Petrenko <lexusgt@yandex.ru>
2019-06-21 15:08:33 +0300

    Кнопка вверх, БД, шаблон страницы

--- page.php
+++ page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <?php the_post(); ?>
 <!-- End Header -->
-<section class="page-head">
+<section class="page-head mod">
     <div class="container">
         <div class="row">
-            <div class="col-12 page-head-center">
+            <div class="col-12">
                 <h1 class="mod"><?php the_title(); ?></h1>
                 <?php speert_the_excerpt($post); ?>
             </div>
@@ -26,32 +26,8 @@
             ?>
         </div>
         <div class="row">
-            <div class="col-lg-8 offset-lg-2 page-content-text">
-
-                <!-- <?php the_content(); ?> -->
-                <div class="adress-wrap">
-                    <div class="adress-item">
-                        <div class="adress-icon adress-icon-phone"></div>
-                        <h4>Phone:</h4>
-                        <p><a href="tel:+12345678910">+ 1 (234) 56 - 789 - 10</a></p>
-                    </div>
-                    <div class="adress-item">
-                    <div class="adress-icon adress-icon-adress"></div>
-                        <h4>Adress:</h4>
-                        <p>Los Angeles, St. Avenue, La 2540</p>
-                    </div>
-                    <div class="adress-item">
-                        <div class="adress-icon adress-icon-email"></div>
-                        <h4>E-mail:</h4>
-                        <p><a href="mailto:info@speertwp.com">info@speertwp.com</a></p>
-                    </div>
-                </div>
-                <div class="adress-form">
-                 <?php
-                    echo do_shortcode ('[contact-form-7 id="194" title="contact form"]')
-                 ?>
-                </div>
-
+            <div class="col-lg-8 offset-lg-2 page-content-text mod">
+                <?php the_content(); ?>
             </div>
         </div>
     </div>
